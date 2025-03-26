44899476fade0ae5f80294d1baf1f70e4436a878
AntonOsipov99 <antonosipov@gmx.de>
2025-03-26 22:33:33 +0100

    added title to the movies on dashboard

--- src/app/start-page/log-in/log-in.component.ts
+++ src/app/start-page/log-in/log-in.component.ts
@@ -34,6 +34,23 @@ export class LogInComponent {
       localStorage.setItem('token', data.token); this.router.navigate(['/movie-dashboard']); return true;
     } catch (error) {
       this.toastr.error('Connection error. Please try again.'); return false;
-    } 
+    }
+  }
+
+  async guestLogIn() {
+    alert('If you want to use this Guest Log In function, then register an account, delete the Alert() and comment on the code under the Alert() again and enter the data of the registration under email and password in the guestLogIn function.');
+    //   try {
+    //     const response = await fetch('http://127.0.0.1:8000/api/auth/login/', {
+    //       method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    //       body: JSON.stringify({ email: 'your registrated email', password: 'your registrated password' })
+    //     });
+    //     const data = await response.json();
+    //     if (!response.ok) { this.toastr.error('Please check your entries and try again.'); return false; }
+    //     if (!data.is_active) { this.toastr.error('Your account is not activated. Please check your emails and click on the activation link.'); return false; }
+    //     localStorage.setItem('token', data.token); this.router.navigate(['/movie-dashboard']); return true;
+    //   } catch (error) {
+    //     this.toastr.error('Connection error. Please try again.'); return false;
+    //   } 
+    // }
   }
 }
